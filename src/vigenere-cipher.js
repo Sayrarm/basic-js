044e280fd32d9b94ca1c1a22835afb174cfbbb0d
Olga D. <minsecond@mail.ru>
2024-12-24 18:48:56 +0300

    feat: add solutions

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  _process(text, key, isEncrypt) {
+    if (typeof text !== 'string' || typeof key !== 'string') {
+      throw new Error("Incorrect arguments!");
+    }
+
+    text = text.toUpperCase();
+    key = key.toUpperCase();
+    let result = "";
+    let keyIndex = 0;
+
+    for (let i = 0; i < text.length; i++) {
+      const char = text[i];
+      const charIndex = this.alphabet.indexOf(char);
+
+      if (charIndex === -1) {
+        result += char;
+      } else {
+        const keyChar = key[keyIndex % key.length];
+        const keyCharIndex = this.alphabet.indexOf(keyChar);
+        const shift = isEncrypt ? keyCharIndex : -keyCharIndex;
+        const newIndex = (charIndex + shift + this.alphabet.length) % this.alphabet.length;
+        result += this.alphabet[newIndex];
+        keyIndex++;
+      }
+    }
+
+    return this.isDirect ? result : result.split("").reverse().join("");
+  }
+  
+  encrypt(text, key) {
+    return this._process(text, key, true);
+  }
+  decrypt(text, key) {
+    return this._process(text, key, false);
   }
 }
 
